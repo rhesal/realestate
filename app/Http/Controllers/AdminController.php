1e3ebfc27de93a7bd7d5b0eb8ccae7611e54f17e
Rhesal Mahadyanto <rhesal.mahadyanto@gmail.com>
2024-04-03 15:29:21 +0700

    Profile
    -

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AdminController extends Controller
+{
+    public function AdminDashboard(){
+        return view('admin.index');
+    }
+
+    public function AdminLogout(Request $request) {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('admin/login');
+    }
+
+    public function AdminLogin() {
+        return view('admin.admin_login');
+    }
+
+    public function AdminProfile() {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view', compact('profileData'));
+    }
+
+    public function AdminProfileStore(Request $request) {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'),$filename);
+            $data['photo'] = $filename;
+        }
+
+        $data->save();
+
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfuly',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    }
+}
---
+++ resources/views/admin/admin_profile_view.blade.php
@@ -0,0 +1,108 @@
+@extends('admin.admin_dashboard')
+@section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+<div class="page-content">
+
+    <div class="row profile-body">
+    <!-- left wrapper start -->
+        <div class="d-none d-md-block col-md-4 col-xl-4 left-wrapper">
+            <div class="rounded card">
+                <div class="card-body">
+                    <div class="mb-2 d-flex align-items-center justify-content-between">
+                        <div>
+                            <img class="wd-100 rounded-circle" src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" alt="profile">
+                            <span class="h4 ms-3">{{ $profileData->username }}</span>
+                        </div>
+                    </div>
+                    <div class="mt-3">
+                        <label class="mb-0 tx-11 fw-bolder text-uppercase">Name:</label>
+                        <p class="text-muted">{{ $profileData->name }}</p>
+                    </div>
+                    <div class="mt-3">
+                        <label class="mb-0 tx-11 fw-bolder text-uppercase">Email:</label>
+                        <p class="text-muted">{{ $profileData->email }}</p>
+                    </div>
+                    <div class="mt-3">
+                        <label class="mb-0 tx-11 fw-bolder text-uppercase">Phone:</label>
+                        <p class="text-muted">{{ $profileData->phone }}</p>
+                    </div>
+                    <div class="mt-3">
+                        <label class="mb-0 tx-11 fw-bolder text-uppercase">Address:</label>
+                        <p class="text-muted">{{ $profileData->address }}</p>
+                    </div>
+                    <div class="mt-3 d-flex social-links">
+                        <a href="javascript:;" class="border btn btn-icon btn-xs me-2">
+                            <i data-feather="github"></i>
+                        </a>
+                        <a href="javascript:;" class="border btn btn-icon btn-xs me-2">
+                            <i data-feather="twitter"></i>
+                        </a>
+                        <a href="javascript:;" class="border btn btn-icon btn-xs me-2">
+                        <i data-feather="instagram"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!-- left wrapper end -->
+    <!-- middle wrapper start -->
+        <div class="col-md-8 col-xl-8 middle-wrapper">
+            <div class="row">
+                <div class="card">
+                    <div class="card-body">
+                        <h6 class="card-title">Update Admin Profile</h6>
+                        <form method="POST" action="{{ route('admin.profile.store') }}" class="forms-sample" enctype="multipart/form-data">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="exampleInputUsername1" class="form-label">Username</label>
+                                <input type="text" class="form-control" name="username" id="exampleInputUsername1" autocomplete="off" value="{{ $profileData->username }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Name</label>
+                                <input type="text" class="form-control" name="name" id="exampleInputUsername1" autocomplete="off" value="{{ $profileData->name }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Email</label>
+                                <input type="email" class="form-control" name="email" id="exampleInputUsername1" autocomplete="off" value="{{ $profileData->email }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Phone</label>
+                                <input type="text" class="form-control" name="phone" id="exampleInputUsername1" autocomplete="off" value="{{ $profileData->phone }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Address</label>
+                                <input type="text" class="form-control" name="address" id="exampleInputUsername1" autocomplete="off" value="{{ $profileData->address }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Photo</label>
+                                <input type="file" class="form-control" name="photo" id="image">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label"></label>
+                                <img id="showImage" class="wd-100 rounded-circle" src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" alt="profile">
+                            </div>
+
+
+                            <button type="submit" class="btn btn-primary me-2">Save Changes</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!-- middle wrapper end -->
+    </div>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
+@endsection
