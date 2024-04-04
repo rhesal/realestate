f40fbcb33b1169625358be4465d9cc98097c765b
Rhesal Mahadyanto <rhesal.mahadyanto@gmail.com>
2024-04-04 11:15:00 +0700

    Upload & Replace

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -40,12 +40,6 @@
                     </div>
                 </div>
             </li>
-
-            @php
-                $id = Auth::user()->id;
-                $data = App\Models\User::find(id);
-            @endphp
-
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="messageDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i data-feather="mail"></i>
@@ -186,18 +180,24 @@
                     </div>
                 </div>
             </li>
+
+            @php
+                $id = Auth::user()->id;
+                $profileData = App\Models\User::find($id);
+            @endphp
+
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="wd-30 ht-30 rounded-circle" src="https://via.placeholder.com/30x30" alt="profile">
+                    <img class="wd-30 ht-30 rounded-circle" src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" alt="profile">
                 </a>
                 <div class="p-0 dropdown-menu" aria-labelledby="profileDropdown">
                     <div class="px-5 py-3 d-flex flex-column align-items-center border-bottom">
                         <div class="mb-3">
-                            <img class="wd-80 ht-80 rounded-circle" src="https://via.placeholder.com/80x80" alt="">
+                            <img class="wd-80 ht-80 rounded-circle" src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" alt="">
                         </div>
                         <div class="text-center">
-                            <p class="tx-16 fw-bolder">Amiah Burton</p>
-                            <p class="tx-12 text-muted">amiahburton@gmail.com</p>
+                            <p class="tx-16 fw-bolder">{{ $profileData->name }}</p>
+                            <p class="tx-12 text-muted">{{ $profileData->email }}</p>
                         </div>
                     </div>
                     <ul class="p-1 list-unstyled">
